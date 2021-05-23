da56ef2f7baaa6b58c21e92b2754e73ec6e55eed
Maciej Olędzki <maciej.oledzki@gmail.com>
2021-05-23 09:08:41 +0200

    WIP: trying to figure out promise loop :)

--- google-photos/src/controllers/AuthController.ts
+++ google-photos/src/controllers/AuthController.ts
@@ -1,23 +1,63 @@
 import { google, oauth2_v2 } from 'googleapis';
-import { GetTokenResponse } from 'google-auth-library/build/src/auth/oauth2client'
+import { GetTokenResponse, GetAccessTokenResponse } from 'google-auth-library/build/src/auth/oauth2client'
 import fs = require('fs');
 import { Request, Response, Application } from "express";
+import Photos = require('googlephotos');
+
+
 
 export class AuthController {
-    private oauth2Client = new google.auth.OAuth2("487815936298-etevasfeipui82f2t73aa2lhef0rbfu5.apps.googleusercontent.com", "process.env.PHOTOS_CLIENT_SECRETS", "http://localhost:3000/oauth2entry");
+    private oauth2Client = new google.auth.OAuth2("487815936298-etevasfeipui82f2t73aa2lhef0rbfu5.apps.googleusercontent.com", process.env.PHOTOS_CLIENT_SECRETS, "http://localhost:3000/oauth2entry");
 
+    private readonly CONFIG_FILE = ".oauth2.json";
+    constructor() {
+        try {
+            let tokens = JSON.parse(fs.readFileSync(this.CONFIG_FILE).toString());
+            this.oauth2Client.setCredentials(tokens)
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
     public processAuthCode(req: Request, res: Response) {
         let code: string = req.query.code as string;
-
+        console.log(code);
         if (code == null) {
             res.status(500).send({
                 message: 'Code parameter missing!'
             });
         } else {
             this.oauth2Client.getToken(code).then((value: GetTokenResponse) => {
-                fs.writeFileSync(".oauth2.json", JSON.stringify(value.tokens));
+                fs.writeFileSync(this.CONFIG_FILE, JSON.stringify(value.tokens));
+                res.status(200).send("OK");
+            }, (reason:any)=> {
+                console.log(reason);
+                res.status(500).send(reason);
             });
         }
     }
 
+    public login(req: Request, res: Response) {
+
+        const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
+
+        const url = this.oauth2Client.generateAuthUrl({
+            // 'online' (default) or 'offline' (gets refresh_token)
+            access_type: 'offline',
+
+            // If you only need one scope you can pass it as a string
+            scope: scopes,
+        });
+        console.log(url);
+        res.redirect(url);
+
+        // Send the user to the url from above. Once they grant access they will be redirected to the
+        // the redirect URL above with a query param code in the redirect. Use the code below to get the
+        // access token.
+
+
+    }
+    public getAuthToken(): Promise<GetAccessTokenResponse> {
+        return this.oauth2Client.getAccessToken();
+    }
 }
--- google-photos/src/routes/Routes.ts
+++ google-photos/src/routes/Routes.ts
@@ -1,59 +1,69 @@
 
 import { Request, Response, Application } from "express";
- import { AuthController } from "../controllers/AuthController";
+import { AuthController } from "../controllers/AuthController";
+import { GetTokenResponse, GetAccessTokenResponse } from 'google-auth-library/build/src/auth/oauth2client'
 
 
 import Photos = require('googlephotos');
+import { oauth2 } from "googleapis/build/src/apis/oauth2";
 
 
 export class Routes {
-    private authController:AuthController = new AuthController();
+    private authController: AuthController = new AuthController();
 
+
+    public processAlbums(result: Array<any>, photos) {
+        photos.albums.list(50).then(albums=> {
+            result.push(albums.albums);
+            if(albums.nextPageToken != null) {
+                this.processAlbums(result, )
+            }
+        })
+    }
     public routes(app: Application): void {
         app.route('/oauth2entry')
-        .get(this.authController.processAuthCode);
+            .get(this.authController.processAuthCode.bind(this.authController));
+        app.route('/login')
+            .get((req: Request, res: Response) => this.authController.login(req, res));
+        app.route('/albums').get((req: Request, res: Response) => {
+            this.authController.getAuthToken().then((token: GetAccessTokenResponse) => {
+                let access_token=token.token;
+                const photos = new Photos(access_token);
+
+                photos.albums.list(50).then(albums=> {
+                    res.status(200).send(albums);
+                });
+            });
+            
+            
+                // const response = await photos.albums.list(50);
+                // allAlbums.push(response.albums);
+                // console.log(response.nextPageToken);
+                // const response2 = await photos.albums.list(50, response.nextPageToken);
+                // allAlbums.push(response2.albums);
+                // res.status(200).send(allAlbums);
+  
+        });
         app.route('/')
             .get((req: Request, res: Response) => {
-                const code = req.query.code;
-                if (code == null) {
-                    res.status(200).send({
-                        message: 'GET lost!'
-                    });
-                } else {
-                    let allAlbums=[];
-                    oauth2Client.setCredentials(tokens);
-                    google.
+                // let allAlbums = [];
+                // oauth2Client.setCredentials(tokens);
+                // google.
 
-                    // The token from above can be used to initialize the photos library.
-                    console.log(tokens);
-                    const photos = new Photos(tokens.access_token);
-                    const response = await photos.albums.list(50);
-                    allAlbums.push(response.albums);
-                    console.log(response.nextPageToken);
-                    const response2 = await photos.albums.list(50, response.nextPageToken);
-                    allAlbums.push(response2.albums);
-                    res.status(200).send(allAlbums);
-
-                }
-            });
+                //     // The token from above can be used to initialize the photos library.
+                //     console.log(tokens);
+                // const photos = new Photos(tokens.access_token);
+                // const response = await photos.albums.list(50);
+                // allAlbums.push(response.albums);
+                // console.log(response.nextPageToken);
+                // const response2 = await photos.albums.list(50, response.nextPageToken);
+                // allAlbums.push(response2.albums);
+                // res.status(200).send(allAlbums);
+                res.status(200).send("OK");
+            }
+            );
         app.route('/albums').get((req: Request, res: Response) => {
 
-
-
-            const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING	];
-
-            const url = oauth2Client.generateAuthUrl({
-                // 'online' (default) or 'offline' (gets refresh_token)
-                access_type: 'offline',
-
-                // If you only need one scope you can pass it as a string
-                scope: scopes,
-            });
-            res.redirect(url);
-
-            // Send the user to the url from above. Once they grant access they will be redirected to the
-            // the redirect URL above with a query param code in the redirect. Use the code below to get the
-            // access token.
 
 
         });
